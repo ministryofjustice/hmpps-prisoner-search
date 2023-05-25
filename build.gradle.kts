a48b29f5bcc1812ef373afcc9852b0c9f896e309
Peter Phillips <peter@greenthistle.co.uk>
2023-05-25 09:45:17 +0100

    SDIT-822: ⬆️ Upgrade to latest spring boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0-beta-2"
   kotlin("plugin.spring") version "1.8.21"
 }
 
