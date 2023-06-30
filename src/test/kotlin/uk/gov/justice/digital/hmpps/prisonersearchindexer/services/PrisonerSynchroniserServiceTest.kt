f665ce57082f3df26ab748b28c2319d2d4f2ecfd
Peter Phillips <peter@greenthistle.co.uk>
2023-06-30 14:27:27 +0100

    SDIT-405: ♻️  Refactor prisoner synchroniser to be just about the individual prisoners

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearchindexer/services/PrisonerSynchroniserServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearchindexer/services/PrisonerSynchroniserServiceTest.kt
@@ -12,12 +12,9 @@ import org.mockito.kotlin.eq
 import org.mockito.kotlin.isA
 import org.mockito.kotlin.isNull
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
-import uk.gov.justice.digital.hmpps.prisonersearchindexer.config.IndexBuildProperties
-import uk.gov.justice.digital.hmpps.prisonersearchindexer.config.TelemetryEvents
 import uk.gov.justice.digital.hmpps.prisonersearchindexer.model.OffenderBookingBuilder
 import uk.gov.justice.digital.hmpps.prisonersearchindexer.model.SyncIndex.GREEN
 import uk.gov.justice.digital.hmpps.prisonersearchindexer.repository.PrisonerRepository
@@ -34,7 +31,6 @@ internal class PrisonerSynchroniserServiceTest {
     nomisService,
     restrictedPatientService,
     incentivesService,
-    IndexBuildProperties(10, 0),
   )
 
   @Nested
@@ -74,209 +70,6 @@ internal class PrisonerSynchroniserServiceTest {
       service.synchronisePrisoner("X12345", GREEN)
 
       verify(telemetryClient).trackEvent(eq("PRISONER_UPDATED"), any(), isNull())
-    }
-  }
-
-  @Nested
-  inner class CheckExistsAndReset {
-    @Nested
-    inner class IndexExists {
-      @BeforeEach
-      internal fun setUp() {
-        whenever(prisonerRepository.doesIndexExist(GREEN))
-          .thenReturn(true)
-          .thenReturn(false)
-
-        service.checkExistsAndReset(GREEN)
-      }
-
-      @Test
-      internal fun `will delete the index if it exists`() {
-        verify(prisonerRepository).deleteIndex(GREEN)
-      }
-
-      @Test
-      internal fun `will recreate the index`() {
-        verify(prisonerRepository).createIndex(GREEN)
-      }
-    }
-
-    @Nested
-    inner class IndexDoesNotExists {
-      @BeforeEach
-      internal fun setUp() {
-        whenever(prisonerRepository.doesIndexExist(GREEN))
-          .thenReturn(false)
-          .thenReturn(false)
-
-        service.checkExistsAndReset(GREEN)
-      }
-
-      @Test
-      internal fun `won't bother deleting index if it does not exist`() {
-        whenever(prisonerRepository.doesIndexExist(GREEN)).thenReturn(false)
-
-        service.checkExistsAndReset(GREEN)
-
-        verify(prisonerRepository, never()).deleteIndex(any())
-      }
-
-      @Test
-      internal fun `will create the index`() {
-        verify(prisonerRepository).createIndex(GREEN)
-      }
-    }
-
-    @Nested
-    inner class IndexDeleteIsSlow {
-
-      @Test
-      fun `waits for index to be deleted before recreating`() {
-        whenever(prisonerRepository.doesIndexExist(GREEN))
-          .thenReturn(true)
-          .thenReturn(true)
-          .thenReturn(false)
-
-        service.checkExistsAndReset(GREEN)
-
-        verify(prisonerRepository, times(3)).doesIndexExist(GREEN)
-        verify(prisonerRepository).deleteIndex(GREEN)
-        verify(prisonerRepository).createIndex(GREEN)
-      }
-    }
-  }
-
-  @Nested
-  inner class SplitAllPrisonersIntoChunks {
-    @Test
-    internal fun `will split total list by our page size`() {
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(30)
-
-      val chunks = service.splitAllPrisonersIntoChunks()
-      assertThat(chunks).containsExactly(
-        PrisonerPage(0, 10),
-        PrisonerPage(1, 10),
-        PrisonerPage(2, 10),
-      )
-    }
-
-    @Test
-    internal fun `will round up last page to page size`() {
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(31)
-
-      var chunks = service.splitAllPrisonersIntoChunks()
-      assertThat(chunks).containsExactly(
-        PrisonerPage(0, 10),
-        PrisonerPage(1, 10),
-        PrisonerPage(2, 10),
-        PrisonerPage(3, 10),
-      )
-
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(29)
-
-      chunks = service.splitAllPrisonersIntoChunks()
-      assertThat(chunks).containsExactly(
-        PrisonerPage(0, 10),
-        PrisonerPage(1, 10),
-        PrisonerPage(2, 10),
-      )
-    }
-
-    @Test
-    internal fun `will create a large number of pages for a large number of prisoners`() {
-      val service = PrisonerSynchroniserService(
-        prisonerRepository,
-        telemetryClient,
-        nomisService,
-        restrictedPatientService,
-        incentivesService,
-        IndexBuildProperties(1000, 0),
-      )
-
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(2000001)
-
-      val chunks = service.splitAllPrisonersIntoChunks()
-      assertThat(chunks).hasSize(2001)
-    }
-
-    @Test
-    internal fun `will create a single pages for a tiny number of prisoners`() {
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(1)
-
-      val chunks = service.splitAllPrisonersIntoChunks()
-      assertThat(chunks).hasSize(1)
-    }
-
-    @Test
-    internal fun `will send a telemetry event`() {
-      val service = PrisonerSynchroniserService(
-        prisonerRepository,
-        telemetryClient,
-        nomisService,
-        restrictedPatientService,
-        incentivesService,
-        IndexBuildProperties(1000, 0),
-      )
-
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(1)
-
-      service.splitAllPrisonersIntoChunks()
-
-      verify(telemetryClient).trackEvent(
-        TelemetryEvents.POPULATE_PRISONER_PAGES.name,
-        mapOf("totalNumberOfPrisoners" to "1", "pageSize" to "1000"),
-        null,
-      )
-    }
-
-    @Test
-    internal fun `will create no pages for no prisoners`() {
-      val service = PrisonerSynchroniserService(
-        prisonerRepository,
-        telemetryClient,
-        nomisService,
-        restrictedPatientService,
-        incentivesService,
-        IndexBuildProperties(1000, 0),
-      )
-
-      whenever(nomisService.getTotalNumberOfPrisoners()).thenReturn(0)
-
-      val chunks = service.splitAllPrisonersIntoChunks()
-      assertThat(chunks).hasSize(0)
-    }
-  }
-
-  @Nested
-  inner class GetAllPrisonerIdentifiersInPage {
-    @BeforeEach
-    internal fun setUp() {
-      whenever(nomisService.getPrisonerNumbers(any(), any())).thenReturn(
-        listOf(
-          "X12345",
-          "X12346",
-          "X12347",
-          "X12348",
-        ),
-      )
-    }
-
-    @Test
-    internal fun `will pass through page numbers`() {
-      service.getAllPrisonerNumbersInPage(PrisonerPage(3, 1000))
-      verify(nomisService).getPrisonerNumbers(3, 1000)
-    }
-
-    @Test
-    internal fun `will map each identifier`() {
-      val prisoners = service.getAllPrisonerNumbersInPage(PrisonerPage(3, 1000))
-
-      assertThat(prisoners).containsExactly(
-        "X12345",
-        "X12346",
-        "X12347",
-        "X12348",
-      )
     }
   }
 }
