a37d56742b02564b6bf585b29699bff715cb0b0b
petergphillips <peter@greenthistle.co.uk>
2023-07-10 08:17:45 +0100

    SDIT-405: ✨ Add in helm lint and fix test reports (#45)
    * SDIT-405: ✨ Add in helm lint and fix test reports
    
    * SDIT-405: ✨ Different approach to path filtering
    
    * SDIT-405: ✨ Add back in build docker and deploy

--- model/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/diff/PrisonerDifferenceService.kt
+++ model/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/diff/PrisonerDifferenceService.kt
@@ -14,13 +14,9 @@ enum class DiffCategory {
   IDENTIFIERS, PERSONAL_DETAILS, ALERTS, STATUS, LOCATION, SENTENCE, RESTRICTED_PATIENT, INCENTIVE_LEVEL, PHYSICAL_DETAILS
 }
 
-data class Difference(val property: String, val categoryChanged: DiffCategory, val oldValue: Any?, val newValue: Any?)
-
-typealias PrisonerDifferences = Map<DiffCategory, List<Difference>>
-
 internal fun getDiffResult(prisoner: Prisoner, other: Prisoner): DiffResult<Prisoner> =
   DiffBuilder(prisoner, other, ToStringStyle.JSON_STYLE).apply {
     Prisoner::class.members
-      .filter { property -> property.findAnnotations<DiffableProperty>().isNotEmpty() }
+      .filter { it.findAnnotations<DiffableProperty>().isNotEmpty() }
       .forEach { property -> append(property.name, property.call(prisoner), property.call(other)) }
   }.build()
