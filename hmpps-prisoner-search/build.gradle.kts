0d36b2d6cdd5a932c6369b1be5a01395ab8b2039
petergphillips <peter@greenthistle.co.uk>
2023-07-10 11:53:54 +0100

    SDIT 937 initial search config (#46)
    * SDIT-937 Add initial prisoner search files
    
    * SDIT-937: ✨ Add in spring security configuration
    
    ---------
    
    Co-authored-by: karenmillermoj <karen.miller@digital.justice.gov.uk>

---
+++ hmpps-prisoner-search/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.2"
+  kotlin("plugin.spring") version "1.8.22"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:1.2.0")
+  implementation("com.google.code.gson:gson:2.10.1")
+
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-cache")
+
+  implementation("org.springframework.data:spring-data-elasticsearch:5.1.1")
+
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.27.0")
+
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk9")
+  implementation("io.arrow-kt:arrow-core:1.1.5")
+  implementation("org.awaitility:awaitility-kotlin:4.2.0")
+
+  implementation(project(":model"))
+
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.16")
+  testImplementation("javax.xml.bind:jaxb-api:2.3.1")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.38.0")
+  testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.3.3")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "19"
+    }
+  }
+}
