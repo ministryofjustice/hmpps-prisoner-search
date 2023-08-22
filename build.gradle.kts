34c876f6d534ccd5fc2471f19da4e463ed750a08
petergphillips <peter@greenthistle.co.uk>
2023-08-22 11:02:35 +0100

    SDIT-405: ⬆️ Bump versions to deploy search to preprod (#107)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0-beta" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0" apply false
   kotlin("plugin.spring") version "1.9.0" apply false
 }
