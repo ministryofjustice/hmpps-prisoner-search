3f44cc33a2522cdf38672fade42cc33c6be733b7
Peter Phillips <peter@greenthistle.co.uk>
2023-09-18 09:56:42 +0100

    SDIT-410: ✨ Add in publishing of domain events

--- hmpps-prisoner-search-indexer/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/config/DiffProperties.kt
+++ hmpps-prisoner-search-indexer/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/config/DiffProperties.kt
@@ -3,4 +3,4 @@ package uk.gov.justice.digital.hmpps.prisonersearch.indexer.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "diff")
-data class DiffProperties(val events: Boolean, val host: String)
+data class DiffProperties(val events: Boolean, val host: String, val prefix: String)
