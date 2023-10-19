6fe768f680ca6cd42ab9c9c545d22416b9ff0081
petergphillips <peter@greenthistle.co.uk>
2023-10-19 17:27:10 +0100

    SDIT-1164: 🔒️ Upgrade spring boot for tomcat vulnerability (#163)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.6.0" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.7.0" apply false
   kotlin("plugin.spring") version "1.9.10" apply false
 }
