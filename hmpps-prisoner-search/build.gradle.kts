35f0e67687745a2b1030cc22987479fc10384dda
petergphillips <peter@greenthistle.co.uk>
2023-11-17 12:19:18 +0000

    SDIT-1255: ⬆️ Upgrade for reactor netty http vulnerability (#192)

--- hmpps-prisoner-search/build.gradle.kts
+++ hmpps-prisoner-search/build.gradle.kts
@@ -11,7 +11,7 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.opensearch.client:spring-data-opensearch-starter:1.2.0")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:1.2.1")
   implementation("com.google.code.gson:gson:2.10.1")
 
   implementation("org.springframework.boot:spring-boot-starter-security")
@@ -38,8 +38,8 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.18") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.18")
-  testImplementation("org.wiremock:wiremock:3.2.0")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.19")
+  testImplementation("org.wiremock:wiremock:3.3.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
