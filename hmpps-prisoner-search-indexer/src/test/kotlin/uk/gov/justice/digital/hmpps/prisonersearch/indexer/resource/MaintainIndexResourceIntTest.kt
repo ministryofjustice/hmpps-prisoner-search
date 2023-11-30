9dc35cbcd0a4c2d03a463a9882175bf4c96949e9
petergphillips <peter@greenthistle.co.uk>
2023-11-30 16:20:46 +0000

    SDIT-1262: ✅ Test that message received before add mapping is not added to newly created index (#199)

--- hmpps-prisoner-search-indexer/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/resource/MaintainIndexResourceIntTest.kt
+++ hmpps-prisoner-search-indexer/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/resource/MaintainIndexResourceIntTest.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.indexer.resource
 
 import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -8,30 +11,50 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import org.mockito.Mockito.CALLS_REAL_METHODS
+import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.reset
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.doThrow
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.http.MediaType
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest
 import uk.gov.justice.digital.hmpps.prisonersearch.common.model.IndexState.BUILDING
 import uk.gov.justice.digital.hmpps.prisonersearch.common.model.IndexState.CANCELLED
 import uk.gov.justice.digital.hmpps.prisonersearch.common.model.IndexState.COMPLETED
 import uk.gov.justice.digital.hmpps.prisonersearch.common.model.IndexStatus
+import uk.gov.justice.digital.hmpps.prisonersearch.common.model.SyncIndex
 import uk.gov.justice.digital.hmpps.prisonersearch.common.model.SyncIndex.GREEN
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.PrisonerBuilder
+import uk.gov.justice.digital.hmpps.prisonersearch.indexer.readResourceAsText
+import uk.gov.justice.digital.hmpps.prisonersearch.indexer.repository.PrisonerRepository
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.BuildAlreadyInProgressException
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.BuildNotInProgressException
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.MaintainIndexService
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.NoActiveIndexesException
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.PrisonerNotFoundException
-import uk.gov.justice.digital.hmpps.prisonersearch.indexer.wiremock.PrisonApiExtension
+import uk.gov.justice.digital.hmpps.prisonersearch.indexer.wiremock.IncentivesApiExtension.Companion.incentivesApi
+import uk.gov.justice.digital.hmpps.prisonersearch.indexer.wiremock.PrisonApiExtension.Companion.prisonApi
 
+@AutoConfigureWebTestClient(timeout = "PT60M")
 class MaintainIndexResourceIntTest : IntegrationTestBase() {
   @SpyBean
   private lateinit var maintainIndexService: MaintainIndexService
+
+  @Autowired
+  @Qualifier("offenderqueue-sqs-client")
+  private lateinit var offenderQueueSqsClient: SqsAsyncClient
+
+  @SpyBean
+  lateinit var prisonerSpyBeanRepository: PrisonerRepository
 
   @BeforeEach
   fun `reset mocks`() = reset(maintainIndexService)
@@ -87,6 +110,38 @@ class MaintainIndexResourceIntTest : IntegrationTestBase() {
         .jsonPath("$.otherIndexState").isEqualTo("BUILDING")
 
       verify(maintainIndexService).prepareIndexForRebuild()
+    }
+
+    @Test
+    fun `Test that messages received during index rebuild don't get added to the index before the mapping is added`() {
+      prisonApi.stubOffenders(PrisonerBuilder("A5678FB"))
+      incentivesApi.stubCurrentIncentive()
+
+      doAnswer {
+        val message = "/messages/offenderDetailsChanged.json".readResourceAsText().replace("A7089FD", "A5678FB")
+        offenderQueueSqsClient.sendMessage(
+          SendMessageRequest.builder().queueUrl(offenderQueueUrl).messageBody(message).build(),
+        ).get()
+
+        Thread.sleep(1000)
+
+        // now call the real method to add the mapping
+        prisonerSpyBeanRepository.addMapping(it.arguments[0] as SyncIndex)
+        CALLS_REAL_METHODS
+      }.doCallRealMethod().whenever(prisonerSpyBeanRepository).addMapping(any())
+
+      webTestClient.put()
+        .uri("/maintain-index/build")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_PRISONER_INDEX")))
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.otherIndex").isEqualTo("GREEN")
+        .jsonPath("$.otherIndexState").isEqualTo("BUILDING")
+
+      verify(maintainIndexService).prepareIndexForRebuild()
+      await untilCallTo { indexQueueService.getNumberOfMessagesCurrentlyOnIndexQueue() } matches { it!! == 0 }
     }
 
     @Test
@@ -207,7 +262,7 @@ class MaintainIndexResourceIntTest : IntegrationTestBase() {
   inner class IndexPrisoner {
     @Test
     fun `Request to index prisoner is successful and calls service`() {
-      PrisonApiExtension.prisonApi.stubOffenders(PrisonerBuilder("A1234BC"))
+      prisonApi.stubOffenders(PrisonerBuilder("A1234BC"))
       buildAndSwitchIndex(GREEN, 1)
 
       webTestClient.put()
