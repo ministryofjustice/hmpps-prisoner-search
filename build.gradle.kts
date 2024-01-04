a2df3689aab4e2b195928c7440f815ffc58dbfaf
Peter Phillips <peter@greenthistle.co.uk>
2024-01-04 11:23:58 +0000

    SDIT-1388: 🐛 Use latest plugin to get owasp working again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.12.0" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.13.0" apply false
   kotlin("plugin.spring") version "1.9.22" apply false
 }
