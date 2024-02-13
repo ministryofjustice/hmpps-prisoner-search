5c31200760b0f3337386c5824c52d6342cd61724
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-13 14:07:52 +0000

    SDIT-1490 Design a basic attribute search request

---
+++ hmpps-prisoner-search/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/search/services/dto/AttributeSearchRequest.kt
@@ -0,0 +1,60 @@
+package uk.gov.justice.digital.hmpps.prisonersearch.search.services.dto
+
+import java.time.LocalDate
+
+data class AttributeSearchRequest(
+  val matchers: List<Matcher>,
+)
+
+data class Matcher(
+  val joinType: JoinType,
+  val textMatchers: List<TextMatcher>? = null,
+  val booleanMatchers: List<BooleanMatcher>? = null,
+  val integerMatchers: List<IntegerMatcher>? = null,
+  val dateMatchers: List<DateMatcher>? = null,
+  val bodyPartMatchers: List<BodyPartMatcher>? = null,
+  val children: List<Matcher>? = null,
+)
+
+data class TextMatcher(
+  val attribute: String,
+  val condition: TextCondition,
+  val searchTerm: String,
+)
+
+data class BooleanMatcher(
+  val attribute: String,
+  val condition: Boolean,
+)
+
+data class IntegerMatcher(
+  val attribute: String,
+  val minValue: Int? = null,
+  val minInclusive: Boolean = true,
+  val maxValue: Int? = null,
+  val maxInclusive: Boolean = true,
+)
+
+data class DateMatcher(
+  val attribute: String,
+  val minValue: LocalDate? = null,
+  val minInclusive: Boolean = true,
+  val maxValue: LocalDate? = null,
+  val maxInclusive: Boolean = true,
+)
+
+data class BodyPartMatcher(
+  val attribute: String,
+  val bodyPart: String? = null,
+  val comment: String? = null,
+)
+
+enum class JoinType {
+  AND,
+  OR,
+}
+
+enum class TextCondition {
+  IS,
+  IS_NOT,
+}
---
+++ hmpps-prisoner-search/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/search/services/dto/AttributeSearchRequestTest.kt
@@ -0,0 +1,649 @@
+package uk.gov.justice.digital.hmpps.prisonersearch.search.services.dto
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jsonMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class AttributeSearchRequestTest {
+
+  private val objectMapper = jsonMapper { addModules(kotlinModule(), JavaTimeModule()) }
+
+  @Test
+  fun `firstName is John`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+            "matchers": [
+              {
+                "joinType": "AND",
+                "textMatchers": [
+                  {
+                    "attribute": "firstName",
+                    "condition": "IS",
+                    "searchTerm": "John"
+                  }
+                ]
+              }
+            ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND lastName is Smith`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+            "matchers": [
+              {
+                "joinType": "AND",
+                "textMatchers": [
+                  {
+                    "attribute": "firstName",
+                    "condition": "IS",
+                    "searchTerm": "John"
+                  },
+                  {
+                    "attribute": "lastName",
+                    "condition": "IS",
+                    "searchTerm": "Smith"
+                  }
+                ]
+              }
+            ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+              TextMatcher(
+                attribute = "lastName",
+                condition = TextCondition.IS,
+                searchTerm = "Smith",
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND (lastName is Smith OR lastName is Jones)`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+            "matchers": [
+              {
+                "joinType": "AND",
+                "textMatchers": [
+                  {
+                    "attribute": "firstName",
+                    "condition": "IS",
+                    "searchTerm": "John"
+                  }
+                ],
+                "children" : [
+                  {
+                    "joinType": "OR",
+                    "textMatchers": [
+                      {
+                        "attribute": "lastName",
+                        "condition": "IS",
+                        "searchTerm": "Smith"
+                      },
+                      {
+                        "attribute": "lastName",
+                        "condition": "IS",
+                        "searchTerm": "Jones"
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            children = listOf(
+              Matcher(
+                joinType = JoinType.OR,
+                textMatchers = listOf(
+                  TextMatcher(
+                    attribute = "lastName",
+                    condition = TextCondition.IS,
+                    searchTerm = "Smith",
+                  ),
+                  TextMatcher(
+                    attribute = "lastName",
+                    condition = TextCondition.IS,
+                    searchTerm = "Jones",
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `(firstName is John AND lastName is Smith) OR (firstName is Jack AND lastName is Smythe)`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+                {
+                  "joinType": "OR",
+                  "children": [
+                    {
+                      "joinType": "AND",
+                      "textMatchers": [
+                        {
+                          "attribute": "firstName",
+                          "condition": "IS",
+                          "searchTerm": "John"
+                        },
+                        {
+                          "attribute": "lastName",
+                          "condition": "IS",
+                          "searchTerm": "Smith"
+                        }
+                      ]
+                    },
+                    {
+                      "joinType": "AND",
+                      "textMatchers": [
+                        {
+                          "attribute": "firstName",
+                          "condition": "IS",
+                          "searchTerm": "Jack"
+                        },
+                        {
+                          "attribute": "lastName",
+                          "condition": "IS_NOT",
+                          "searchTerm": "Jones"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.OR,
+            children = listOf(
+              Matcher(
+                joinType = JoinType.AND,
+                textMatchers = listOf(
+                  TextMatcher(
+                    attribute = "firstName",
+                    condition = TextCondition.IS,
+                    searchTerm = "John",
+                  ),
+                  TextMatcher(
+                    attribute = "lastName",
+                    condition = TextCondition.IS,
+                    searchTerm = "Smith",
+                  ),
+                ),
+              ),
+              Matcher(
+                joinType = JoinType.AND,
+                textMatchers = listOf(
+                  TextMatcher(
+                    attribute = "firstName",
+                    condition = TextCondition.IS,
+                    searchTerm = "Jack",
+                  ),
+                  TextMatcher(
+                    attribute = "lastName",
+                    condition = TextCondition.IS_NOT,
+                    searchTerm = "Jones",
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND recall is true`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "booleanMatchers": [
+                   {
+                     "attribute": "recall",
+                     "condition": true
+                   }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            booleanMatchers = listOf(
+              BooleanMatcher(
+                attribute = "recall",
+                condition = true,
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND heightCentimetres GTE 150`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "integerMatchers": [
+                   {
+                     "attribute": "heightCentimetres",
+                     "minValue": 150 
+                   }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            integerMatchers = listOf(
+              IntegerMatcher(
+                attribute = "heightCentimetres",
+                minValue = 150,
+                minInclusive = true,
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND shoeSize between 11 and 12`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "integerMatchers": [
+                   {
+                     "attribute": "shoeSize",
+                     "minValue": 11,
+                     "maxValue": 12
+                   }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            integerMatchers = listOf(
+              IntegerMatcher(
+                attribute = "shoeSize",
+                minValue = 11,
+                minInclusive = true,
+                maxValue = 12,
+                maxInclusive = true,
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND receptionDate after 2023-01-01 AND releaseDate before 2024-01-01`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "dateMatchers": [
+                   {
+                     "attribute": "receptionDate",
+                     "minValue": "2023-01-01"
+                   },
+                    {
+                      "attribute": "releaseDate",
+                      "maxValue": "2024-01-01",
+                      "maxInclusive": false
+                    }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            dateMatchers = listOf(
+              DateMatcher(
+                attribute = "receptionDate",
+                minValue = LocalDate.parse("2023-01-01"),
+              ),
+              DateMatcher(
+                attribute = "releaseDate",
+                maxValue = LocalDate.parse("2024-01-01"),
+                maxInclusive = false,
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND receptionDate is 2023-01-01`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "dateMatchers": [
+                   {
+                     "attribute": "receptionDate",
+                     "minValue": "2023-01-01",
+                     "maxValue": "2023-01-01"
+                   }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            dateMatchers = listOf(
+              DateMatcher(
+                attribute = "receptionDate",
+                minValue = LocalDate.parse("2023-01-01"),
+                minInclusive = true,
+                maxValue = LocalDate.parse("2023-01-01"),
+                maxInclusive = true,
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND tattoo on shoulder is dragon`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "bodyPartMatchers": [
+                   {
+                     "attribute": "tattoo",
+                     "bodyPart": "shoulder",
+                     "comment": "dragon"
+                   }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            bodyPartMatchers = listOf(
+              BodyPartMatcher(
+                attribute = "tattoo",
+                bodyPart = "shoulder",
+                comment = "dragon",
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+
+  @Test
+  fun `firstName is John AND (scar on face OR scar on head)`() {
+    val request = objectMapper.readValue<AttributeSearchRequest>(
+      """{
+             "matchers": [
+               {
+                 "joinType": "AND",
+                 "textMatchers": [
+                   {
+                     "attribute": "firstName",
+                     "condition": "IS",
+                     "searchTerm": "John"
+                   }
+                 ],
+                 "children" : [
+                   {
+                     "joinType": "OR",
+                     "bodyPartMatchers": [
+                       {
+                         "attribute": "scar",
+                         "bodyPart": "face"
+                       },
+                       {
+                         "attribute": "scar",
+                         "bodyPart": "head"
+                       }
+                     ]
+                   }
+                 ]
+               }
+             ]
+          }
+      """.trimIndent(),
+    )
+
+    assertThat(request).isEqualTo(
+      AttributeSearchRequest(
+        matchers = listOf(
+          Matcher(
+            joinType = JoinType.AND,
+            textMatchers = listOf(
+              TextMatcher(
+                attribute = "firstName",
+                condition = TextCondition.IS,
+                searchTerm = "John",
+              ),
+            ),
+            children = listOf(
+              Matcher(
+                joinType = JoinType.OR,
+                bodyPartMatchers = listOf(
+                  BodyPartMatcher(
+                    attribute = "scar",
+                    bodyPart = "face",
+                  ),
+                  BodyPartMatcher(
+                    attribute = "scar",
+                    bodyPart = "head",
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    )
+  }
+}
