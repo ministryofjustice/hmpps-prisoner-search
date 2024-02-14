9d5847463abe0a09ce5d7f3b261b51ca9ef7c68b
Peter Phillips <peter@greenthistle.co.uk>
2024-02-14 09:29:28 +0000

    SDIT-1500: 🔒️ Upgrade to latest plugin to suppress nimbus JWT vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.2" apply false
   kotlin("plugin.spring") version "1.9.22" apply false
 }
