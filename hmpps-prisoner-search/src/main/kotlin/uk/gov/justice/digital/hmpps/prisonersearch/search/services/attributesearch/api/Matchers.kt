4a32cfeb51a9673814bd96576cdb1abc7212af7b
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-19 12:38:00 +0000

    SDIT-1490 Don't use a temporary list, use collection extensions

--- hmpps-prisoner-search/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/search/services/attributesearch/api/Matchers.kt
+++ hmpps-prisoner-search/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/search/services/attributesearch/api/Matchers.kt
@@ -8,34 +8,27 @@ data class Matchers(
   val children: List<Matchers>? = null,
 ) {
   fun validate() {
-    if (typeMatchers().isEmpty() && children.isNullOrEmpty()) {
+    if (matchers.isNullOrEmpty() && children.isNullOrEmpty()) {
       throw AttributeSearchException("Matchers must not be empty")
     }
   }
-
-  fun typeMatchers(): List<TypeMatcher<*>> =
-//    listOfNotNull(stringMatchers, booleanMatchers, intMatchers, dateMatchers, dateTimeMatchers)
-//      .flatten()
-    matchers ?: emptyList()
 }
 
-fun List<Matchers>.getAllMatchers(): List<Matchers> {
-  val allMatchers = mutableListOf<Matchers>()
-  forEach {
-    allMatchers.add(it)
-    it.children?.also { children -> allMatchers.addAll(children.getAllMatchers()) }
-  }
-  return allMatchers
-}
+fun List<Matchers>.getAllMatchers(): List<Matchers> =
+  fold<Matchers, MutableList<Matchers>>(mutableListOf()) { allMatchers, matcher ->
+    allMatchers.apply {
+      add(matcher)
+      addAll(matcher.children?.getAllMatchers() ?: emptyList())
+    }
+  }.toList()
 
-fun List<Matchers>.getAllTypeMatchers(): List<TypeMatcher<*>> {
-  val allMatchers = mutableListOf<TypeMatcher<*>>()
-  forEach {
-    allMatchers.addAll(it.typeMatchers())
-    it.children?.also { children -> allMatchers.addAll(children.getAllTypeMatchers()) }
-  }
-  return allMatchers
-}
+fun List<Matchers>.getAllTypeMatchers(): List<TypeMatcher<*>> =
+  fold<Matchers, MutableList<TypeMatcher<*>>>(mutableListOf()) { allTypeMatchers, matcher ->
+    allTypeMatchers.apply {
+      addAll(matcher.matchers ?: emptyList())
+      addAll(matcher.children?.getAllTypeMatchers() ?: emptyList())
+    }
+  }.toList()
 
 enum class JoinType {
   AND,
