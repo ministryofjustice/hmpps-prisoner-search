382ee5dce49f37f3839c41e4e2e0b99d6f88734a
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-09-20 11:17:58 +0100

    SDIT-2049: ⬆️ Upgrade to hmpps spring boot 6.0.6 for path directory traversal fix (#421)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.5" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6" apply false
   kotlin("plugin.spring") version "2.0.20" apply false
 }
