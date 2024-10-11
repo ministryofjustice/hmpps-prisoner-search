3e93d2a6e5eed77399acf037640ced4b31e7fdbf
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-11 09:33:28 +0100

    SDIT-2107: ⬆️ Upgrade to latest versions and suppress non issues (#434)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   implementation("org.springframework.data:spring-data-elasticsearch:5.3.4")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.opensearch.client:spring-data-opensearch-starter:1.5.2")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:1.5.3")
 }
 
 kotlin {
