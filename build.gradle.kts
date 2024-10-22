2f4ed4076da24458f2a6956eab2a38cdb9f9cd7f
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-22 13:55:23 +0100

    SDIT-2141: ⬆️ Upgrade to hmpps spring boot 6.0.7 for path directory traversal suppression (#439)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.7" apply false
   kotlin("plugin.spring") version "2.0.20" apply false
 }
