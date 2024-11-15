52bc70eca01fe708e787eccf8dd358ebbffc8715
steverendell <steve.rendell@syscon.net>
2024-11-15 11:28:49 +0000

    SDIT-2182 Create duplicate logging from the red index updates

--- hmpps-prisoner-search-indexer/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/services/PrisonerDifferencesService.kt
+++ hmpps-prisoner-search-indexer/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/services/PrisonerDifferencesService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.indexer.services
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.repository.PrisonerDifferences
@@ -13,10 +14,21 @@ import java.time.temporal.ChronoUnit
 class PrisonerDifferencesService(
   private val prisonerDifferencesRepository: PrisonerDifferencesRepository,
 ) {
+  private companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   fun retrieveDifferences(label: PrisonerDifferencesLabel, from: Instant, to: Instant): List<PrisonerDifferences> =
     prisonerDifferencesRepository.findByLabelAndDateTimeBetween(label, from, to)
 
   @Transactional
-  fun deleteOldData(): Int =
-    prisonerDifferencesRepository.deleteByDateTimeBefore(Instant.now().minus(28, ChronoUnit.DAYS))
+  fun deleteOldData(): Int {
+    val threshold = Instant.now().minus(28, ChronoUnit.DAYS)
+    log.info("Deleting old prisoner differences before $threshold")
+
+    val rows = prisonerDifferencesRepository.deleteByDateTimeBefore(threshold)
+
+    log.info("Deleted $rows old prisoner differences")
+    return rows
+  }
 }
