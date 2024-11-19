645e1f69143a4aa1295f9959b7541e1562e23d6d
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-19 16:20:24 +0000

    SDIT-2265: ⬆️  Upgrade spring boot plugin for netty upgrade (#454)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9" apply false
   kotlin("plugin.spring") version "2.0.21" apply false
 }
