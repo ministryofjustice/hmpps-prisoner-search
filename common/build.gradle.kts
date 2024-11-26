2571b90b976fbcac3bbd2dde8f7e5d1560ef54cd
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-26 11:25:26 +0000

    SDIT-2318: ⬆️  Upgrade spring boot to 3.4.0 (#458)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,7 +11,7 @@ dependencyCheck {
 
 dependencies {
   implementation("org.springframework.data:spring-data-elasticsearch:5.4.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.opensearch.client:spring-data-opensearch-starter:1.5.3")
 }
