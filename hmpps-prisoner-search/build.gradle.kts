82c78e4c705ba5201296680c5ecc876e1d4d6648
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-02-20 12:01:56 +0000

    SDIT-2593 Upgrade to full verion of kotlin lib

--- hmpps-prisoner-search/build.gradle.kts
+++ hmpps-prisoner-search/build.gradle.kts
@@ -27,7 +27,7 @@ testing {
 configurations["testSmokeImplementation"].extendsFrom(configurations["testImplementation"])
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0-beta-2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0")
   implementation("org.opensearch.client:spring-data-opensearch-starter:1.6.1")
   implementation("com.google.code.gson:gson:2.12.1")
   implementation("org.springframework.boot:spring-boot-starter-cache")
