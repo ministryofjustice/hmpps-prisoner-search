21994f8a7599ed99e224ff6e68d4ff95d6b418ef
steverendell <steve.rendell@syscon.net>
2025-03-28 16:16:02 +0000

    SDIT-2177 Move alerts to domain partial update 2

--- hmpps-prisoner-search-indexer/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/wiremock/AlertsApiMockServer.kt
+++ hmpps-prisoner-search-indexer/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/wiremock/AlertsApiMockServer.kt
@@ -26,73 +26,81 @@ class AlertsApiMockServer : WireMockServer(8097) {
   fun stubSuccess(
     prisonerNumber: String = "A1234AA",
     alertCodes: List<Pair<String, String>> = listOf("A" to "ABC"),
+    createdDates: List<String> = emptyList(),
+    activeFromDates: List<String> = emptyList(),
   ) {
-    val alerts = alertCodes.joinToString(",") { (type, code) ->
-      """{
-      "alertUuid": "${UUID.randomUUID()}",
-      "prisonNumber": "$prisonerNumber",
-      "alertCode": {
-        "alertTypeCode": "$type",
-        "alertTypeDescription": "Type Description for $type",
-        "code": "$code",
-        "description": "Code description for $code"
-      },
-      "description": "Alert description",
-      "authorisedBy": "A. Nurse, An Agency",
-      "activeFrom": "2021-09-27",
-      "activeTo": "2022-07-15",
-      "isActive": true,
-      "createdAt": "2021-09-27T14:19:25",
-      "createdBy": "USER1234",
-      "createdByDisplayName": "Firstname Lastname",
-      "lastModifiedAt": "2022-07-15T15:24:56",
-      "lastModifiedBy": "USER1234",
-      "lastModifiedByDisplayName": "Firstname Lastname",
-      "activeToLastSetAt": "2022-07-15T15:24:56",
-      "activeToLastSetBy": "USER1234",
-      "activeToLastSetByDisplayName": "Firstname Lastname",
-      "prisonCodeWhenCreated": "LEI"
-    }"""
+    val alerts = alertCodes.mapIndexed { index, (type, code) ->
+      alertJson(prisonerNumber, type, code, createdDates.getOrNull(index), activeFromDates.getOrNull(index))
     }
+      .joinToString(",")
 
     stubFor(
       get(urlPathMatching("/prisoners/$prisonerNumber/alerts"))
         .willReturn(
-          okJson(
-            """
-            {
-              "totalPages": 1,
-              "totalElements": ${alertCodes.size},
-              "first": true,
-              "last": true,
-              "size": ${alertCodes.size},
-              "content": [ $alerts ],
-              "number": 0,
-              "sort": {
-                "empty": true,
-                "sorted": true,
-                "unsorted": true
-              },
-              "numberOfElements": ${alertCodes.size},
-              "pageable": {
-                "offset": 0,
-                "sort": {
-                  "empty": false,
-                  "sorted": false,
-                  "unsorted": true
-                },
-                "unpaged": false,
-                "pageSize": 1000,
-                "paged": true,
-                "pageNumber": 0
-              },
-              "empty": false
-            }
-            """.trimIndent(),
-          ),
+          okJson(resultJson(alertCodes.size, alerts)),
         ),
     )
   }
+
+  private fun alertJson(prisonerNumber: String, type: String, code: String, createdAt: String?, activeFrom: String?): String = """{
+        "alertUuid": "${UUID.randomUUID()}",
+        "prisonNumber": "$prisonerNumber",
+        "alertCode": {
+          "alertTypeCode": "$type",
+          "alertTypeDescription": "Type Description for $type",
+          "code": "$code",
+          "description": "Code description for $code"
+        },
+        "description": "Alert description",
+        "authorisedBy": "A. Nurse, An Agency",
+        "activeFrom": "${activeFrom ?: "2022-07-15"}",
+        "activeTo": "2022-07-15",
+        "isActive": true,
+        "createdAt": "${createdAt ?: "2021-09-27T14:19:25"}",
+        "createdBy": "USER1234",
+        "createdByDisplayName": "Firstname Lastname",
+        "lastModifiedAt": "2022-07-15T15:24:56",
+        "lastModifiedBy": "USER1234",
+        "lastModifiedByDisplayName": "Firstname Lastname",
+        "activeToLastSetAt": "2022-07-15T15:24:56",
+        "activeToLastSetBy": "USER1234",
+        "activeToLastSetByDisplayName": "Firstname Lastname",
+        "prisonCodeWhenCreated": "LEI"
+      }"""
+
+  private fun resultJson(
+    size: Int,
+    alerts: String,
+  ): String = """
+    {
+      "totalPages": 1,
+      "totalElements": $size,
+      "first": true,
+      "last": true,
+      "size": $size,
+      "content": [ $alerts ],
+      "number": 0,
+      "sort": {
+        "empty": true,
+        "sorted": true,
+        "unsorted": true
+      },
+      "numberOfElements": $size,
+      "pageable": {
+        "offset": 0,
+        "sort": {
+          "empty": false,
+          "sorted": false,
+          "unsorted": true
+        },
+        "unpaged": false,
+        "pageSize": 1000,
+        "paged": true,
+        "pageNumber": 0
+      },
+      "empty": false
+    }
+  """.trimIndent()
 }
 
 class AlertsApiExtension :
