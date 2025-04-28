48ae24fa4c493059b9a0c0429fcc0d7c8ae88a5c
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-04-28 09:39:54 +0100

    SDIT-2733: ⬆️  Upgrade hmpps spring boot plugin to 8.1.0

--- hmpps-prisoner-search-indexer/build.gradle.kts
+++ hmpps-prisoner-search-indexer/build.gradle.kts
@@ -28,20 +28,20 @@ testing {
 configurations["testSmokeImplementation"].extendsFrom(configurations["testImplementation"])
 
 dependencies {
-  implementation("org.opensearch.client:spring-data-opensearch-starter:1.6.2")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:1.6.3")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.3")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-cache")
 
-  implementation("org.springframework.data:spring-data-elasticsearch:5.4.4")
+  implementation("org.springframework.data:spring-data-elasticsearch:5.4.5")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.4")
   // Leaving at version 2.9.0 to match App Insights https://github.com/microsoft/ApplicationInsights-Java/blob/3.6.2/dependencyManagement/build.gradle.kts#L16
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.13.1")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.14.0")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
@@ -53,23 +53,23 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.7.5")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.2")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.25") {
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.3")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.26") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.28")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.30")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.0")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:4.1.0")
-  testImplementation("com.google.code.gson:gson:2.12.1")
+  testImplementation("com.google.code.gson:gson:2.13.1")
   testImplementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.10.0")
 }
 
 kotlin {
   jvmToolchain(21)
   compilerOptions {
-    freeCompilerArgs.add("-Xwhen-guards")
+    freeCompilerArgs.addAll("-Xjvm-default=all", "-Xwhen-guards")
   }
 }
 
