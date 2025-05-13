0518815962a7dad915ddce3a01706c7af734774c
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2025-05-13 08:22:06 +0100

    SDIT-2742 Add care needs, smoking and languages fields to prisoner-search fix (#530)
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- common/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/common/nomis/OffenderLanguageDto.kt
+++ common/src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/common/nomis/OffenderLanguageDto.kt
@@ -3,8 +3,8 @@ package uk.gov.justice.digital.hmpps.prisonersearch.common.nomis
 data class OffenderLanguageDto(
   val type: String,
   val code: String,
-  val readSkill: String,
-  val writeSkill: String,
-  val speakSkill: String,
+  val readSkill: String? = null,
+  val writeSkill: String? = null,
+  val speakSkill: String? = null,
   val interpreterRequested: Boolean,
 )
