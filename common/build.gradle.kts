26e050e020a9176101319e16568b1012b9410c45
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-06-12 16:17:12 +0100

    SDIT-2793 Upgrades and remove client tracking client (#565)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,9 +11,9 @@ dependencyCheck {
 
 dependencies {
   implementation("org.springframework.data:spring-data-elasticsearch:5.5.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.opensearch.client:spring-data-opensearch-starter:1.6.4")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:1.7.0")
 }
 
 kotlin {
--- hmpps-prisoner-search/build.gradle.kts
+++ hmpps-prisoner-search/build.gradle.kts
@@ -27,35 +27,35 @@ testing {
 configurations["testSmokeImplementation"].extendsFrom(configurations["testImplementation"])
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.5")
-  implementation("org.opensearch.client:spring-data-opensearch-starter:1.6.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.6")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:1.7.0")
   implementation("com.google.code.gson:gson:2.13.1")
   implementation("org.springframework.boot:spring-boot-starter-cache")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.data:spring-data-elasticsearch:5.5.0")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
   // Leaving at version 2.9.0 to match App Insights https://github.com/microsoft/ApplicationInsights-Java/blob/3.6.2/dependencyManagement/build.gradle.kts#L16
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.14.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.16.0")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk9")
   implementation("org.awaitility:awaitility-kotlin:4.3.0")
 
-  implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
+  implementation("com.github.ben-manes.caffeine:caffeine:3.2.1")
 
   implementation(project(":common"))
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.5")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.6")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.28") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.29") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.30")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.0")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.31")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:4.1.1")
