b02c87ec5de23fbf08029de199fb326c123a8497
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-06-12 15:38:48 +0100

    SDIT-2793 Go to swagger versions that don't break open API docs tests

--- hmpps-prisoner-search/build.gradle.kts
+++ hmpps-prisoner-search/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.29") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.32")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.31")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
