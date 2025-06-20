47420994c2e439e8d5b12e2d5871a6484eaa6ab9
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-06-20 14:26:28 +0100

    SDIT-2879 Upgrade to Spring Boot 3.5.3 (#567)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0-beta" apply false
   kotlin("plugin.spring") version "2.1.21" apply false
 }
