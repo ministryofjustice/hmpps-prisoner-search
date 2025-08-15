843be428ad985704c5d6eef823a0dff72173db5c
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-08-15 15:15:47 +0100

    SDIT-2997: ⬆️  Upgrade for netty fix & kotlin 2.2.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.5" apply false
-  kotlin("plugin.spring") version "2.2.0" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.6" apply false
+  kotlin("plugin.spring") version "2.2.10" apply false
 }
