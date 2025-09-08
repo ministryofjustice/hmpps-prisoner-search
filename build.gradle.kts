8b854f717b82a44ace6c0dff4de3b6f90d53564f
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-09-08 11:54:10 +0100

    SDIT-3027: ⬆️  Upgrade hmpps spring boot plugin for netty dos fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.1" apply false
   kotlin("plugin.spring") version "2.2.10" apply false
 }
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,7 +11,7 @@ dependencyCheck {
 
 dependencies {
   implementation("org.springframework.data:spring-data-elasticsearch:5.5.3")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.12")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.opensearch.client:spring-data-opensearch-starter:2.0.1")
 }
