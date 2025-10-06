ad44141c3faf32f624affe5d85ab387197807591
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-10-06 16:53:47 +0100

    SDIT-2804 Ignore expected errors for some batch jobs (#652)

--- hmpps-prisoner-search-indexer/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/batch/BatchManagerTest.kt
+++ hmpps-prisoner-search-indexer/src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/indexer/batch/BatchManagerTest.kt
@@ -3,17 +3,23 @@ package uk.gov.justice.digital.hmpps.prisonersearch.indexer.batch
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.boot.test.system.OutputCaptureExtension
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.event.ContextRefreshedEvent
+import uk.gov.justice.digital.hmpps.prisonersearch.common.model.IndexStatus
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.batch.BatchType.CHECK_INDEX_COMPLETE
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.batch.BatchType.COMPARE_INDEX_SIZE
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.batch.BatchType.FULL_INDEX_REFRESH
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.batch.BatchType.REMOVE_OLD_DIFFERENCES
+import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.BuildAlreadyInProgressException
+import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.BuildNotInProgressException
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.CompareIndexService
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.MaintainIndexService
 import uk.gov.justice.digital.hmpps.prisonersearch.indexer.services.PrisonerDifferencesService
@@ -45,6 +51,33 @@ class BatchManagerTest {
   }
 
   @Test
+  fun `should ignore expected errors in the check index complete service`() = runTest {
+    whenever(maintainIndexService.markIndexingComplete())
+      .thenThrow(BuildNotInProgressException(IndexStatus("some-id").toBuildInProgress()))
+    val batchManager = batchManager(CHECK_INDEX_COMPLETE)
+
+    assertDoesNotThrow {
+      batchManager.onApplicationEvent(event)
+    }
+
+    verify(maintainIndexService).markIndexingComplete()
+    verify(context).close()
+  }
+
+  @Test
+  fun `should not ignore unexpected error in the check index complete service`() = runTest {
+    whenever(maintainIndexService.markIndexingComplete()).thenThrow(RuntimeException("error"))
+    val batchManager = batchManager(CHECK_INDEX_COMPLETE)
+
+    assertThrows<RuntimeException> {
+      batchManager.onApplicationEvent(event)
+    }
+
+    verify(maintainIndexService).markIndexingComplete()
+    verify(context, never()).close()
+  }
+
+  @Test
   fun `should call the compare index size service`() = runTest {
     val batchManager = batchManager(COMPARE_INDEX_SIZE)
 
@@ -52,6 +85,19 @@ class BatchManagerTest {
 
     verify(compareIndexService).doIndexSizeCheck()
     verify(context).close()
+  }
+
+  @Test
+  fun `should not ignore errors in the compare index size service`() = runTest {
+    whenever(compareIndexService.doIndexSizeCheck()).thenThrow(RuntimeException("error"))
+    val batchManager = batchManager(COMPARE_INDEX_SIZE)
+
+    assertThrows<RuntimeException> {
+      batchManager.onApplicationEvent(event)
+    }
+
+    verify(compareIndexService).doIndexSizeCheck()
+    verify(context, never()).close()
   }
 
   @Test
@@ -65,13 +111,54 @@ class BatchManagerTest {
   }
 
   @Test
-  fun `should call the remove old indexes service`() = runTest {
+  fun `should ignore expected errors in the refresh index service`() = runTest {
+    whenever(refreshIndexService.startIndexRefresh())
+      .thenThrow(BuildAlreadyInProgressException(IndexStatus("some-id").toBuildInProgress()))
+    val batchManager = batchManager(FULL_INDEX_REFRESH)
+
+    assertDoesNotThrow {
+      batchManager.onApplicationEvent(event)
+    }
+
+    verify(refreshIndexService).startIndexRefresh()
+    verify(context).close()
+  }
+
+  @Test
+  fun `should not ignore unexpected errors in the refresh index service`() = runTest {
+    whenever(refreshIndexService.startIndexRefresh())
+      .thenThrow(RuntimeException("error"))
+    val batchManager = batchManager(FULL_INDEX_REFRESH)
+
+    assertThrows<RuntimeException> {
+      batchManager.onApplicationEvent(event)
+    }
+
+    verify(refreshIndexService).startIndexRefresh()
+    verify(context, never()).close()
+  }
+
+  @Test
+  fun `should call the remove old differences service`() = runTest {
     val batchManager = batchManager(REMOVE_OLD_DIFFERENCES)
 
     batchManager.onApplicationEvent(event)
 
     verify(prisonerDifferencesService).deleteOldData()
     verify(context).close()
+  }
+
+  @Test
+  fun `should not ignore errors in the remove old differences service`() = runTest {
+    whenever(prisonerDifferencesService.deleteOldData()).thenThrow(RuntimeException("error"))
+    val batchManager = batchManager(REMOVE_OLD_DIFFERENCES)
+
+    assertThrows<RuntimeException> {
+      batchManager.onApplicationEvent(event)
+    }
+
+    verify(prisonerDifferencesService).deleteOldData()
+    verify(context, never()).close()
   }
 
   private fun batchManager(batchType: BatchType) = BatchManager(
