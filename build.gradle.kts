07fe46ebbf57f2b3a3d5521455b4907de8584b87
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-10-06 10:16:50 +0100

    SDIT-3080: 🔒️ Upgrade to hmpps plugin 9.1.2 for logback vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.2" apply false
   kotlin("plugin.spring") version "2.2.20" apply false
 }
--- hmpps-prisoner-search/build.gradle.kts
+++ hmpps-prisoner-search/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.34") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.37")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.38")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
