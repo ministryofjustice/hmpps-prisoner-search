f18ed45830fecfab3955e51235ef43893f8a3dd4
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-11-24 11:27:32 +0000

    SDIT-3216: ⬆️ Upgrade to spring boot 3.5.8 & hmpps plugin 9.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4" apply false
-  kotlin("plugin.spring") version "2.2.20" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.2.0" apply false
+  kotlin("plugin.spring") version "2.2.21" apply false
 }
