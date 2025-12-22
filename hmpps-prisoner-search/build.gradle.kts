cbf77365f68d5c7eab84e7bb5062faf62cc0270e
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-12-22 08:55:02 +0000

    SDIT-3270: 🔧 Use toolchain instead

--- hmpps-prisoner-search/build.gradle.kts
+++ hmpps-prisoner-search/build.gradle.kts
@@ -62,14 +62,10 @@ dependencies {
 }
 
 kotlin {
+  jvmToolchain(25)
   compilerOptions {
     freeCompilerArgs.addAll("-Xjvm-default=all", "-Xwhen-guards", "-Xannotation-default-target=param-property")
   }
-}
-
-java {
-  sourceCompatibility = JavaVersion.VERSION_25
-  targetCompatibility = JavaVersion.VERSION_25
 }
 
 configure<com.gorylenko.GitPropertiesPluginExtension> {
