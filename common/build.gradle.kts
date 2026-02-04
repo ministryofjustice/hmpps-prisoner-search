44e70254620667266da37bff07c7c9b687774c49
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-02-04 07:57:22 +0000

    SDIT-3426: ⬆️ Upgrade to latest sqs lib / spring cloud aws (#702)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   implementation("org.springframework.data:spring-data-elasticsearch:6.0.2")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
   implementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server")
-  implementation("org.opensearch.client:spring-data-opensearch-starter:3.0.0")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:3.0.1")
 }
 
 kotlin {
