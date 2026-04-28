be3e5bd3e8682b35121310fb90ac578990f5d4d0
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-04-28 14:23:34 +0100

    SDIT-3744: ⬆️ Upgrade to latest spring boot 4.0.6 etc. (#763)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,13 +10,14 @@ dependencyCheck {
 }
 
 dependencies {
-  implementation("org.springframework.data:spring-data-elasticsearch:6.0.4")
+  implementation("org.springframework.data:spring-data-elasticsearch:6.0.5")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+  implementation("org.springdoc:springdoc-openapi-starter-common:3.0.2")
   constraints {
     implementation("org.webjars:swagger-ui:5.32.2")
   }
   implementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server")
-  implementation("org.opensearch.client:spring-data-opensearch-starter:3.0.4")
+  implementation("org.opensearch.client:spring-data-opensearch-starter:3.0.5")
 }
 
 kotlin {
