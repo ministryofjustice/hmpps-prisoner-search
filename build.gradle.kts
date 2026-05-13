4456a735025c2d63c00872a6b7963e55af9275c3
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-05-13 13:16:02 +0100

    SDIT-3798: ⬆️ Upgrade gradle plugin for snyk ignore handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.5" apply false
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.0" apply false
   kotlin("plugin.spring") version "2.3.21" apply false
 }
